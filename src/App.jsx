df79968652f473c572228018973d1d9508e99a94
sonalbhayani <bhayanisonal84@gmail.com>
2026-06-17 00:13:33 +0530

    project done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CountryComponent from "./CountryComponent";
+
+function App() {
+  
+
+  return (
+    <>
+     <CountryComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ src/CountryComponent.jsx
@@ -0,0 +1,45 @@
+import {useState,useEffect} from 'react'
+import axios from 'axios'
+
+export default function CountryComponent() {
+    const [data,setData]=useState([]);
+    const [displayData,setDisplayData]=useState([]);
+    const [input,setInput]=useState("");
+    useEffect(()=>{
+        axios.get(" https://countries-search-data-prod-812920491762.asia-south1.run.app/countries")
+        .then((res)=>{setData(res.data)
+            setDisplayData(res.data)
+        })
+        .catch((error)=>console.log(error))
+    },[])
+    useEffect(()=>{
+        if(!input){
+            setDisplayData(data);
+           
+        }else{
+             
+               setDisplayData(data.filter((ele)=>  ele.common.toLowerCase().startsWith(input.toLowerCase())));
+             
+        }
+       
+    },[input])
+   
+  return (
+    <div className='container m-5'>
+        <input type="text" name="input" className='form-control' id="input" value={input} onChange={(e)=>setInput(e.target.value)}/>
+        <div className='row mt-5'>
+            {displayData.map((ele)=>( 
+                <div className='col-1 div-data m-5' key={ele.common} >
+                    <div className="card">
+                    <img src={ele.png} className="card-img-top img" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">{ele.common}</h5>
+                        
+                    </div>
+                    </div>
+                </div>))}
+           
+        </div>
+    </div>
+  )
+}
